6fc922a2268a93e3dc4e8406d313ed1ff6f0d8b1
Gregor Doroschenko <g.doroschenko@gmail.com>
2022-12-08 09:53:24 +0100

    Create first request

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Hier kommen weitere Konfiguration für die app
   await app.listen(3000);
 }
 bootstrap();
